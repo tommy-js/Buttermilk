945cfae98275175e07f262b5b289bd9cb3dacc04
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-06 13:06:32 -0500

    Add create_account and login page routes

---
+++ pages/create_account/index.tsx
@@ -0,0 +1,9 @@
+import { CreateAccountPage } from "../../components/account_management/CreateAccountPage/CreateAccountPage";
+
+export default function CreateAccount() {
+  return (
+    <div>
+      <CreateAccountPage />
+    </div>
+  );
+}
---
+++ pages/login/index.tsx
@@ -0,0 +1,9 @@
+import { LoginPage } from "../../components/account_management/LoginPage/LoginPage";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
