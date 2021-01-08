94692f5877d3757c513b18c91037ace3453a9571
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-08 11:34:03 -0500

    Add a load-more button to stories feed

--- components/home/Feed/Feed.tsx
+++ components/home/Feed/Feed.tsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { FeedItem } from "../feed_item/FeedItem/FeedItem";
+import { LoadMore } from "../LoadMore/LoadMore";
+import {
+  sortByRecent,
+  sortByStars,
+  updateSearchQuery,
+  loadMoreSet,
+} from "./index";
 import styles from "./styles.module.scss";
+
 const testimg = require("../../../public/test_img.jpg");
 const testimg2 = require("../../../public/test_img_2.jpg");
 const testimg3 = require("../../../public/test_img_3.jpg");
@@ -86,42 +94,461 @@ export const Feed: React.FC<Props> = (props) => {
       imgUrl: testimg4,
       stars: 7,
     },
+    {
+      id: 9,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 10,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 11,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 12,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 13,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 14,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 15,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 16,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 17,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 18,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 19,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 20,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 21,
+      title: "Bouldering in the snow",
+      username: "James",
+      timestamp: 1609972233,
+      imgUrl: testimg4,
+      stars: 0,
+    },
+    {
+      id: 22,
+      title: "Skiing for noobs",
+      username: "J22A",
+      timestamp: 1609972261,
+      imgUrl: testimg,
+      stars: 3,
+    },
+    {
+      id: 23,
+      title: "Caving at its best",
+      username: "Tommy",
+      timestamp: 1609972341,
+      imgUrl: testimg2,
+      stars: 8,
+    },
+    {
+      id: 24,
+      title: "Stepping into the unknown",
+      username: "Mac",
+      timestamp: 1609972346,
+      imgUrl: testimg2,
+      stars: 8,
+    },
+    {
+      id: 25,
+      title: "Bouldering in the Gunks",
+      username: "Max",
+      timestamp: 1609972352,
+      imgUrl: testimg4,
+      stars: 2,
+    },
+    {
+      id: 26,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 27,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 28,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 29,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 30,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 31,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 32,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 33,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 34,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 35,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 36,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 37,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 38,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 39,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 40,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 41,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 42,
+      title: "Bouldering in the snow",
+      username: "James",
+      timestamp: 1609972233,
+      imgUrl: testimg4,
+      stars: 0,
+    },
+    {
+      id: 43,
+      title: "Skiing for noobs",
+      username: "J22A",
+      timestamp: 1609972261,
+      imgUrl: testimg,
+      stars: 3,
+    },
+    {
+      id: 44,
+      title: "Caving at its best",
+      username: "Tommy",
+      timestamp: 1609972341,
+      imgUrl: testimg2,
+      stars: 8,
+    },
+    {
+      id: 45,
+      title: "Stepping into the unknown",
+      username: "Mac",
+      timestamp: 1609972346,
+      imgUrl: testimg2,
+      stars: 8,
+    },
+    {
+      id: 46,
+      title: "Bouldering in the Gunks",
+      username: "Max",
+      timestamp: 1609972352,
+      imgUrl: testimg4,
+      stars: 2,
+    },
+    {
+      id: 47,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 48,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 49,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 50,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 51,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 52,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 53,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 54,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 55,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 56,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 57,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 58,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
+    {
+      id: 59,
+      title: "Hiking the Appalacian Trail",
+      username: "Karen",
+      timestamp: 1609972012,
+      imgUrl: testimg2,
+      stars: 64,
+    },
+    {
+      id: 60,
+      title: "Mountaineering",
+      username: "Jord",
+      timestamp: 1609972365,
+      imgUrl: testimg3,
+      stars: 822,
+    },
+    {
+      id: 61,
+      title: "Trees",
+      username: "44ew",
+      timestamp: 1609971245,
+      imgUrl: testimg,
+      stars: 83,
+    },
+    {
+      id: 62,
+      title: "Something else",
+      username: "Tommy",
+      timestamp: 1609972384,
+      imgUrl: testimg4,
+      stars: 7,
+    },
   ];
-  const [array, setArray] = useState(test);
+  const [original] = useState([...test]);
+  const [array, setArray] = useState(test.splice(0, 15));
 
   useEffect(() => {
-    if (props.selected.recent === true) {
-      let arr = [...test];
-      arr.sort(function (a, b) {
-        return a.timestamp - b.timestamp;
-      });
-      setArray(arr);
-    } else if (props.selected.stars === true) {
-      let arr = [...test];
-      arr.sort(function (a, b) {
-        return b.stars - a.stars;
-      });
-      setArray(arr);
-    }
+    if (props.selected.recent === true) setArray(sortByRecent(array));
+    else if (props.selected.stars === true) setArray(sortByStars(array));
   }, [props.selected]);
 
   useEffect(() => {
     if (props.searching === true) {
-      let original = [...test];
-      let updated = [];
-      let queryLowercased = props.searchQuery.toLowerCase();
-      for (let i = 0; i < original.length; i++) {
-        let lowerCasedTitle = original[i].title.toLowerCase();
-        let lowerCasedUsername = original[i].username.toLowerCase();
-        if (lowerCasedTitle.includes(queryLowercased)) {
-          updated.push(original[i]);
-        } else if (lowerCasedUsername.includes(queryLowercased)) {
-          updated.push(original[i]);
-        }
-      }
-      setArray(updated);
+      setArray(updateSearchQuery(array, props.searchQuery));
     }
   }, [props.searchQuery]);
+
+  useEffect(() => {
+    console.log("array: " + array.length);
+    console.log("original: " + original.length);
+  }, [array]);
+
+  function loadMore() {
+    setArray(loadMoreSet(array, original));
+  }
 
   return (
     <div className={styles.feed}>
@@ -136,6 +563,7 @@ export const Feed: React.FC<Props> = (props) => {
           key={el.id}
         />
       ))}
+      <LoadMore loadMore={loadMore} />
     </div>
   );
 };
---
+++ components/home/LoadMore/LoadMore.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+  loadMore: () => void;
+}
+
+export const LoadMore: React.FC<Props> = (props) => {
+  return <button onClick={() => props.loadMore()}>Load More</button>;
+};
