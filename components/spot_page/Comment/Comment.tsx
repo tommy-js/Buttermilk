fd518be02b0abb1769ab269973989a8e08525158
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-08 18:33:04 -0500

    Add spot page star logic

--- components/spot_page/Comment/Comment.tsx
+++ components/spot_page/Comment/Comment.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
+
+const star = require("../../../public/star.png");
+const empty_star = require("../../../public/star_empty.png");
 
 interface Props {
   username: string;
@@ -10,10 +13,39 @@ interface Props {
 }
 
 export const Comment: React.FC<Props> = (props) => {
+  const [liked, setLiked] = useState(false);
+  const [starVal, setStarVal] = useState(props.stars);
+  const [img, setImg] = useState(empty_star);
+
+  useEffect(() => {
+    // Check user model to see if the user has already liked this comment
+  }, []);
+
+  function modStar() {
+    if (liked === false) {
+      let val = starVal;
+      val++;
+      setStarVal(val);
+      setImg(star);
+    } else {
+      let val = starVal;
+      val--;
+      setStarVal(val);
+      setImg(empty_star);
+    }
+    setLiked(!liked);
+  }
+
   return (
     <div className={styles.comment}>
       <p className={styles.username}>{props.username}</p>
       <p className={styles.text}>{props.text}</p>
+      <div className={styles.interaction}>
+        <p className={styles.value}>{starVal}</p>
+        <div className={styles.star_img} onClick={() => modStar()}>
+          <img className={styles.img} src={img} />
+        </div>
+      </div>
     </div>
   );
 };
