c3f0c5b801ab6f44cf2d7f6e5997909bfd2916a2
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-13 18:35:52 -0500

    Add history link to user profile

--- components/user/HistoryGuide/HistoryGuide.tsx
+++ components/user/HistoryGuide/HistoryGuide.tsx
@@ -1,24 +1,47 @@
 import React from "react";
+import Link from "next/link";
+import { returnDate } from "./index";
 import styles from "./styles.module.scss";
 
 interface Props {
   title: string;
-  timestamp: string;
+  timestamp: number;
   imgUrl: string;
 }
 
 export const HistoryGuide: React.FC<Props> = (props) => {
   return (
-    <div>
-      <div className={styles.left_block}>
-        <div className={styles.image_container}>
-          <img className={styles.image} src={props.imgUrl} />
-        </div>
-      </div>
-      <div className={styles.title_box}>
-        <p className={styles.title}>{props.title}</p>
-        <p className={styles.timestamp}>{props.timestamp}</p>
-      </div>
-    </div>
+    <Link href={`/story/${props.id}`} passHref>
+      <HistoryLink
+        imgUrl={props.imgUrl}
+        title={props.title}
+        timestamp={props.timestamp}
+      />
+    </Link>
   );
 };
+
+const HistoryLink = React.forwardRef(
+  ({ onClick, href, imgUrl, title, timestamp }, ref) => {
+    return (
+      <a
+        className={styles.history_link}
+        href={href}
+        onClick={onClick}
+        ref={ref}
+      >
+        <div className={styles.guide}>
+          <div className={styles.left_block}>
+            <div className={styles.image_container}>
+              <img className={styles.image} src={imgUrl} />
+            </div>
+          </div>
+          <div className={styles.title_box}>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.timestamp}>Posted {returnDate(timestamp)}</p>
+          </div>
+        </div>
+      </a>
+    );
+  }
+);
--- components/user/HistoryStory/HistoryStory.tsx
+++ components/user/HistoryStory/HistoryStory.tsx
@@ -1,25 +1,48 @@
 import React from "react";
-
+import Link from "next/link";
+import { returnDate } from "./index";
 import styles from "./styles.module.scss";
 
 interface Props {
   title: string;
-  timestamp: string;
+  id: string;
+  timestamp: number;
   imgUrl: string;
 }
 
 export const HistoryStory: React.FC<Props> = (props) => {
   return (
-    <div>
-      <div className={styles.left_block}>
-        <div className={styles.image_container}>
-          <img className={styles.image} src={props.imgUrl} />
-        </div>
-      </div>
-      <div className={styles.title_box}>
-        <p className={styles.title}>{props.title}</p>
-        <p className={styles.timestamp}>Posted {props.timestamp}</p>
-      </div>
-    </div>
+    <Link href={`/story/${props.id}`} passHref>
+      <HistoryLink
+        imgUrl={props.imgUrl}
+        title={props.title}
+        timestamp={props.timestamp}
+      />
+    </Link>
   );
 };
+
+const HistoryLink = React.forwardRef(
+  ({ onClick, href, imgUrl, title, timestamp }, ref) => {
+    return (
+      <a
+        className={styles.history_link}
+        href={href}
+        onClick={onClick}
+        ref={ref}
+      >
+        <div className={styles.guide}>
+          <div className={styles.left_block}>
+            <div className={styles.image_container}>
+              <img className={styles.image} src={imgUrl} />
+            </div>
+          </div>
+          <div className={styles.title_box}>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.timestamp}>Posted {returnDate(timestamp)}</p>
+          </div>
+        </div>
+      </a>
+    );
+  }
+);
